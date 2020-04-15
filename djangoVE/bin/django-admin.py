0e31ae9264a0eddaf3389d34d2bf1097a0d9fbb4
chosky <davidhenaoo98@gmail.com>
2020-04-15 15:33:37 -0500

    Se elimina el entorno virtual del repo

--- djangoVE/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jhenaoo/Documents/JoseWrock/P1/taller2/P1-Taller2/djangoVE/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
